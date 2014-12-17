d963507474fcbff1a3fad84d96feaf0b8097c3e9
Vandana Pai <vandanapai01@gmail.com>
2014-12-17 00:09:27 -0800

    nudge up animation to make images move up on hover

---
+++ js/jquery.effects.js
@@ -0,0 +1,26 @@
+// $(document).ready(function () {
+
+
+// 	$(window).scroll(function () {
+// 		var startValue = 1000;
+// 		var stopValue = 1600;
+// 		var scrollTop = $(window).scrollTop();
+
+// 		if (scrollTop > startValue) {
+// 			$({countNum: $('.increase').text()}).animate({countNum: 100}, {
+// 			  duration: 700,
+// 			  easing:'linear',
+// 			  step: function() {
+// 			    $('.increase').text(Math.floor(this.countNum));
+// 			  },
+// 			  complete: function() {
+// 			    $('.increase').text(this.countNum);
+// 			  }
+// 			});
+// 		}
+// 	})
+
+
+
+
+// });
